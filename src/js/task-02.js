5d78a643a3b4b653767783c70387ee5010a71c34
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-05-27 14:25:51 +0200

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredients) => {
+    const listItem = document.createElement("li");
+
+    listItem.classList.add("item");
+
+    listItem.textContent = ingredients;
+
+    list.appendChild(listItem);
+});
+
+console.log(list);
