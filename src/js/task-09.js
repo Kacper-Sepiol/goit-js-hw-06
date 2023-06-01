bf76e5449063ecdf0d0c10b4187536bc4a3b33df
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-06-01 14:24:02 +0200

    made task-9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,17 @@
+"use strict";
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+
+const btnChangeColor = document.querySelector(".change-color");
+const changeColor = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click", () => {
+    changeColor.textContent = getRandomHexColor();
+    body.style.backgroundColor = getRandomHexColor();
+});
