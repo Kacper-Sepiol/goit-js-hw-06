5d2e0202038c6dce0a80671996da7d9cab90a0d6
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-06-01 14:18:30 +0200

    made task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+"use strict";
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const {
+        elements: { password, login },
+    } = event.currentTarget;
+
+    if (login.value === "" || password.value === "") {
+        return console.log("Nie wszystkie pola zostały uzupełnione!");
+    }
+
+    console.log(`Login: ${login}, Password: ${password.value}`);
+    event.currentTarget.reset();
+});
